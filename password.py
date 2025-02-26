fae9b3658981a190b0b6effcd0f6cdd143ed6d06
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-26 21:18:54 +0200

    nothing is being returned.

--- password.py
+++ password.py
@@ -8,9 +8,27 @@
     Has at least one special character
 
 '''
+password = []
+message = []
 specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '/']
 lettersLower = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 lettersCapital = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-password = []
+passwordIsValid = True
 
-
+def is_password_secure(password):
+    if len(password) < 8:
+        passwordIsValid = False
+        message.append('Your password should at least be 8 character long.')
+    elif not specialCharacters in password:
+        passwordIsValid = False
+        message.append('You should at least have 1 special character')
+    elif not lettersCapital in password:
+        passwordIsValid = False
+        message.append('You should at least have 1 capital letter')
+    elif not lettersLower in password:
+        passwordIsValid = False
+        message.append('You should have at least 1 lower character')
+    else:
+        print('Password is valid')
+    return True
+is_password_secure('khazi')
