4b9a8c88bc25c4c334f5fda6053fcefdb86c1ffd
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-27 11:39:58 +0200

    Add the numbers.

--- password.py
+++ password.py
@@ -7,16 +7,13 @@
 
     Has at least one special character
 
-'''
-
 
 
 def is_password_secure():
     password = input('Enter password: ')
     message = ''
     specialCharacters = str(['!', '@', '#', '$', '%', '^', '&', '*', '+', '/'])
-    lettersLower = s
-    tr([chr(i) for i in range(ord('a'), ord('z') + 1)])
+    lettersLower = str([chr(i) for i in range(ord('a'), ord('z') + 1)])
     lettersCapital = str([chr(i) for i in range(ord('A'), ord('Z') + 1)])
     passwordIsValid = True
     if len(password) < 8:
@@ -30,4 +27,31 @@ def is_password_secure():
         print('You should have at least 1 lower character')
     else:
         print('Password is valid')
+is_password_secure()
+'''
+
+def is_password_secure():
+    password = input('Enter password: ')
+    message = ''
+    special_characters = '!@#$%^&*+/'
+    letters_lower = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    letters_upper = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    has_special_char = any(char in special_characters for char in password)
+    has_lower_letter = any(char in letters_lower for char in password)
+    has_upper_letter = any(char in letters_upper for char in password)
+    
+    if len(password) < 8:
+        message += 'Your password should be at least 8 characters long. '
+    if not has_special_char:
+        message += 'Your password should have at least one special character. '
+    if not has_lower_letter:
+        message += 'Your password should have at least one lowercase letter. '
+    if not has_upper_letter:
+        message += 'Your password should have at least one uppercase letter. '
+    
+    if message:
+        print(message)
+    else:
+        print('Password is valid.')
+
 is_password_secure()
